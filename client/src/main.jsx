957a5f561c01c56cf35c74e52633732c329babcc
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-07-30 20:16:03 -0300

    add redux in main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// REACT
+import React from "react";
+// REACT-ROUTER-DOM
+import ReactDOM from "react-dom/client";
+// * FILE APP
+import App from "./App.jsx";
+// CSS
+import "./index.css";
+// * FILE STORE
+import { persistor, store } from "./redux/store.js";
+// REACT-REDUX
+import { Provider } from "react-redux";
+// REDUX-PERSIST
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
