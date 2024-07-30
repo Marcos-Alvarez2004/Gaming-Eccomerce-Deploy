3881115b3ca01c4809563966d13272be7c1d84cf
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-07-30 00:52:48 -0300

    Start api server, connect database and config index

---
+++ api/index.js
@@ -0,0 +1,43 @@
+// DEPENDENCIAS
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+import cookieParser from "cookie-parser";
+// CONFIG DOTENV
+dotenv.config();
+// __DIRNAME
+const __dirname = path.resolve();
+// * CONECT TO DATABASE
+mongoose
+  .connect(
+    "mongodb+srv://marcos:fasdfhaufgaisuo2312@general.wugamgm.mongodb.net/?retryWrites=true&w=majority&appName=General"
+  )
+  .then(console.log("Connected to MongoDB!"))
+  .catch((err) => {
+    console.log(err);
+  });
+// * APP VARIABLE
+const app = express();
+// APP USE
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "/client/dist")));
+// APP GET
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+// ! HANDLE ERROR
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+// * LISTEN SERVER PORT 3000
+app.listen(3000, () => {
+  console.log("Server is running on port 5000!");
+});
