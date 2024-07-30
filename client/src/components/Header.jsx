5b140f79eb01ea12fc004d24e221ca23444ad19b
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-07-30 18:08:23 -0300

    changue dark mode

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,22 +3,22 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-slate-200 shadow-md">
+    <header className="bg-black border-b border-green-600">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-red-500">Gaming</span>
-            <span className="text-red-700">Eccomerce</span>
+            <span className="text-green-400">Gaming</span>
+            <span className="text-green-600">Eccomerce</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form className="py-2 px-5 rounded-lg flex items-center border border-green-600">
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
           <button>
-            <i className="bx bx-search text-red-500"></i>
+            <i className="bx bx-search text-green-600 hover:text-green-400"></i>
           </button>
         </form>
         <ul className="flex gap-4">
