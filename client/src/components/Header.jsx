2c2845089043790cd3fea330bbd714e5083af386
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-07-30 16:55:52 -0300

    header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,39 @@
-import React from "react";
+// REACT-ROUTER-DOM
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-red-500">Gaming</span>
+            <span className="text-red-700">Eccomerce</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <button>
+            <i className="bx bx-search text-red-500"></i>
+          </button>
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
 }
