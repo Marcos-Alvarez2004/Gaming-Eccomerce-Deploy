2442563ce46e19950be6537a2e62b0217bf71af2
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-07-30 18:40:40 -0300

    Sign up in client successfully

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -23,14 +23,10 @@ export default function Header() {
         </form>
         <ul className="flex gap-4">
           <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
-              Home
-            </li>
+            <li className="hidden sm:inline  hover:underline">Home</li>
           </Link>
           <Link to="/about">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
-              About
-            </li>
+            <li className="hidden sm:inline  hover:underline">About</li>
           </Link>
         </ul>
       </div>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,93 @@
-import React from "react";
+import { useState } from "react";
+// REACT-ROUTER-DOM
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  // USESTATE
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  // NAVIGATE
+  const navigate = useNavigate();
+  // HANDLECHANGE
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  // HANDLESUBMIT
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success == false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl  text-center font-semibold my-7">
+        Registrar cuenta
+      </h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Nombre de usuario"
+          className="p-3 rounded-lg bg-white/10"
+          id="username"
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="p-3 rounded-lg bg-white/10"
+          id="email"
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Contraseña (min 4)"
+          className="p-3 rounded-lg bg-white/10"
+          id="password"
+          onChange={handleChange}
+          minLength="4"
+          required
+        />
+
+        <button
+          disabled={loading}
+          className="bg-green-600 mt-8 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Cargando..." : "Registrar"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Tienes una cuenta?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-green-500 hover:underline">Ingresar</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }
