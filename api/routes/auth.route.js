bea495b85221eee6477f42efce3eebf92a5db116
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-07-30 20:00:17 -0300

    route and backend signin user successfully

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 // EXPRESS
 import express from "express";
 // * CONTROLLERS
-import { signup } from "../controllers/auth.controller.js";
+import { signup, signin } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
