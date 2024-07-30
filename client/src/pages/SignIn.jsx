f90d8b50176f98f07fe0e0e57b0f9746cb7c5dc4
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-07-30 20:22:17 -0300

    signin in client successfully

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,83 @@
-import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate("/");
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
+  };
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">
+        Iniciar Sesion
+      </h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="email"
+          className="p-3 rounded-lg bg-white/10"
+          id="email"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="p-3 rounded-lg bg-white/10"
+          id="password"
+          minLength="4"
+          required
+          onChange={handleChange}
+        />
+
+        <button
+          disabled={loading}
+          className="bg-green-600 mt-8 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Cargando..." : "Ingresar"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>No tienes una cuenta creada?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-green-500 hover:underline">Registrar</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }
