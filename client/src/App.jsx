1b426b0b3e77bfee9743edf810cae35129ddbdac
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-07-30 16:10:21 -0300

    Add pages fold and packgage react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+// REACT-ROUTER-DOM
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// PAGES
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Search from "./pages/Search";
 
 const App = () => {
-  return <div className="text-5xl text-center text-fuchsia-700">App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
