36bbf4fa969cdecf8792b78c828496ceb0e84f78
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-08-05 17:38:38 -0300

    add delete listing backend

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,12 +1,16 @@
 // EXPRESS
 import express from "express";
 // * CONTROLLER LISTING
-import { createListing } from "../controllers/listing.controller.js";
+import {
+  createListing,
+  deleteListing,
+} from "../controllers/listing.controller.js";
 // VERIFY TOKEN
 import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
 
 export default router;
