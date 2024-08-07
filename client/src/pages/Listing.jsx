193728656826e4e81b6bb79fb39d3cc4dac58718
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-08-07 19:56:59 -0300

    add bullet controll in listing and styles

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -3,18 +3,23 @@ import { useParams } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { useSelector } from "react-redux";
-import { Navigation } from "swiper/modules";
-import "swiper/css/bundle";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 // https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
 export default function Listing() {
-  SwiperCore.use([Navigation]);
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const params = useParams();
   // const { currentUser } = useSelector((state) => state.user);
+
+  const pagination = {
+    clickable: true,
+  };
 
   useEffect(() => {
     const fetchListing = async () => {
@@ -40,27 +45,33 @@ export default function Listing() {
 
   return (
     <main>
-      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
-      {error && (
-        <p className="text-center my-7 text-2xl">Something went wrong!</p>
-      )}
+      {loading && <p className="text-center my-7 text-2xl">Cargando...</p>}
+      {error && <p className="text-center my-7 text-2xl">Algo salio mal!</p>}
       {listing && !loading && !error && (
-        <div>
-          <Swiper navigation>
-            {listing.imageUrls.map((url) => (
-              <SwiperSlide key={url}>
-                <div
-                  className="h-[550px]"
-                  style={{
-                    background: `url(${url}) center no-repeat`,
-                    backgroundSize: "cover",
-                  }}
-                ></div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-          <div>{listing.name}</div>
-        </div>
+        <>
+          <h1 className="text-center font-bold mt-7 text-2xl p-2 break-all">
+            {listing.name}
+          </h1>
+          <div className="mt-7">
+            <Swiper
+              className="sm:w-1/4 w-full h-full"
+              modules={[Pagination]}
+              pagination={pagination}
+              grabCursor
+            >
+              {listing.imageUrls.map((url) => (
+                <SwiperSlide key={url} className="w-full h-[330px] bg-white">
+                  <img
+                    src={url}
+                    className="h-full w-full select-none bg-white"
+                    alt="imagen de la publicación"
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <section>{listing.name}</section>
+          </div>
+        </>
       )}
     </main>
   );
