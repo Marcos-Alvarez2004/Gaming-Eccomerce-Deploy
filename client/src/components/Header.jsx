32506a8d2403d6bc7267a9ed0efc68f77dbdd7d3
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-08-08 18:51:27 -0300

    Search function complete

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,30 @@
+// REACT
+import { useState, useEffect } from "react";
 // REACT-ROUTER-DOM
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 // REACT-REDUX
 import { useSelector } from "react-redux";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+
+    if (searchTermFromUrl) setSearchTerm(searchTermFromUrl);
+  }, [location.search]);
+
   return (
     <header className="bg-black border-b border-green-600">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -14,11 +34,16 @@ export default function Header() {
             <span className="text-green-600">Eccomerce</span>
           </h1>
         </Link>
-        <form className="py-2 px-5 rounded-lg flex items-center border border-green-600">
+        <form
+          onSubmit={handleSubmit}
+          className="py-2 px-5 rounded-lg flex items-center border border-green-600"
+        >
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <button>
             <i className="bx bx-search text-green-600 hover:text-green-400"></i>
