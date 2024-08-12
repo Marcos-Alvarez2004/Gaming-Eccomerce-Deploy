b50eb0bc90ac2762a35718ca01a79f6c421dcc8e
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-08-12 13:06:38 -0300

    header responsive

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,9 +6,14 @@ import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   const { currentUser } = useSelector((state) => state.user);
   const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
+
+  const handleClickOpen = () => {
+    setIsOpen(!isOpen);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,8 +31,8 @@ export default function Header() {
   }, [location.search]);
 
   return (
-    <header className="bg-black border-b border-green-600">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+    <header className="bg-black px-2 sm:px-0 border-b border-green-600 w-full">
+      <div className="flex justify-between items-center max-w-6xl mx-auto md:p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-green-400">Gaming</span>
@@ -41,7 +46,7 @@ export default function Header() {
           <input
             type="text"
             placeholder="Search..."
-            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            className="bg-transparent focus:outline-none w-36 sm:w-56"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
@@ -50,13 +55,52 @@ export default function Header() {
           </button>
         </form>
         <ul className="flex gap-4">
-          <Link to="/">
-            <li className="hidden sm:inline  hover:underline">Home</li>
+          <div
+            onClick={handleClickOpen}
+            className="md:hidden flex flex-col items-center p-4"
+          >
+            <div className="space-y-2 cursor-pointer">
+              <div
+                className={`w-8 h-0.5 bg-green-400 transition-transform duration-300 ${
+                  isOpen ? "transform rotate-45 translate-y-3" : ""
+                }`}
+              ></div>
+              <div
+                className={`w-8 h-0.5 bg-green-400 transition-opacity duration-300 ${
+                  isOpen ? "opacity-0" : ""
+                }`}
+              ></div>
+              <div
+                className={`w-8 h-0.5 bg-green-400 transition-transform duration-300 ${
+                  isOpen ? "transform -rotate-45 -translate-y-2" : ""
+                }`}
+              ></div>
+            </div>
+          </div>
+
+          {isOpen && (
+            <nav className="absolute md:hidden flex flex-col gap-y-2 py-2 items-center right-0 top-[54px] bg-white/10 rounded-br rounded-bl border-b border-green-600 border-l border-r select-none">
+              <Link to="/" className="inline-block p-1">
+                Inicio
+              </Link>
+              <div className="bg-white/20 h-[.05rem] w-full"></div>
+              <Link to="/about" className="inline-block p-1">
+                Sobre Nosotros
+              </Link>
+              <div className="bg-white/20 h-[.05rem] w-full"></div>
+              <Link to="/profile" className="inline-block p-1 text-green-400">
+                PERFIL
+              </Link>
+            </nav>
+          )}
+
+          <Link to="/" className="hidden md:inline hover:underline">
+            <li>Inicio</li>
           </Link>
-          <Link to="/about">
-            <li className="hidden sm:inline  hover:underline">About</li>
+          <Link to="/about" className="hidden md:inline hover:underline">
+            <li>Sobre Nostros</li>
           </Link>
-          <Link to="/profile">
+          <Link to="/profile" className="hidden md:inline">
             {currentUser ? (
               <img
                 className="rounded-full h-7 w-7 object-cover"
